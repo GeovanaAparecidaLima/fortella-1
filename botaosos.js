c5ab933a4039092a6038fadac1598973ff994f47
ThaynaraCarli <21520230016@maristabrasil.g12.br>
2025-10-15 10:10:26 -0300

    cria funcionamento em javascript botão sos

---
+++ botaosos.js
@@ -0,0 +1,5 @@
+const sosButton = document.getElementById('sosButton');
+
+sosButton.addEventListener('click', () => {
+  alert('⚠️ Pedido de ajuda enviado! A equipe de emergência foi notificada.');
+});
